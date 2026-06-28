34e09519802e2f4f682ed152ec880a59f73342bb
Shubham Shandilya <128954805+ShubhamShandilya0309@users.noreply.github.com>
2026-06-28 14:03:56 +0530

    Add: course-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int>inDeg(numCourses,0);
+        vector<vector<int>>adjLs(numCourses);
+        for(auto it: prerequisites){
+            inDeg[it[0]]++;
+            adjLs[it[1]].push_back(it[0]);
+        }
+        queue<int>q;
+        for(int i=0;i<numCourses;i++){
+            if(inDeg[i]==0){
+                q.push(i);
+            }
+        }
+        vector<int>ans;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+            for(auto it : adjLs[node]){
+                inDeg[it]--;
+                if(inDeg[it]==0){
+                    q.push(it);
+                }
+            }
+        }
+        return (ans.size() == numCourses) ? ans : vector<int>();
+    }
+};
